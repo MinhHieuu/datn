c2418d7ffa3a79a51a128c18adb8b18947a996fe
Minh Hieu <hieulm04z@gmail.com>
2026-02-01 22:17:04 +0700

    .

--- src/main/java/com/beeshop/sd44/Sd44Application.java
+++ src/main/java/com/beeshop/sd44/Sd44Application.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@SpringBootApplication
 @SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
 })
 public class Sd44Application {
 
